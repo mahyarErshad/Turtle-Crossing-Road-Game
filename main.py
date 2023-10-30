2db2a3618e2a74f56ba81b3f52199dde9c17b5d6
mahyarErshad <mahyar.ershad92@gmail.com>
2023-10-30 14:37:21 +0330

    Added game over text

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ screen.tracer(0)
 
 player = Player()
 car = CarManager()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkeypress(player.go_up, "Up")
@@ -24,9 +25,11 @@ while game_is_on:
     for cars in car.allCars:
         if cars.distance(player) < 23:
             game_is_on = False
+            scoreboard.game_over()
 
     if player.is_at_finish_line():
         player.go_to_start()
         car.increase_speed()
+        scoreboard.increase_level()
 
 screen.exitonclick()
